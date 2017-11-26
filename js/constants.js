fea191209f587e2136bc9398f3a521ddb62890d4
cangqiong <cang19910623@163.com>
2017-11-26 23:31:18 +0800

    添加博客诸多功能

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+// const webSitePrefix = "http://localhost:8081";
+const webSitePrefix = "http://www.xiaocanghai.com";
+const imageUploadUrl = webSitePrefix+"/image/upload";
+const saveBlogUrl = webSitePrefix+"/blog/add";
+const updateBlogUrl = webSitePrefix+"/blog/update";
+const deleteBlogUrl = webSitePrefix+"/blog/delete";
+const queryBlogUrl = webSitePrefix+"/blog/query";
+const queryBlogListUrl = webSitePrefix+"/blog/queryBlogList";
